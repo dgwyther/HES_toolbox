09551cde732d692ffa7fe7b803ba06a828ef3346
David Gwyther <david.gwyther@gmail.com>
2020-07-20 16:10:31 +1000

    new reporting method

--- README.md
+++ README.md
@@ -10,11 +10,39 @@ Written and tested with `python3`. Dash works with `python2.7`. Requirements giv
 2. Install requirements with `$ pip install -r requirements.txt`
 3. Voila!
 
+## Toolbox structure
+Comments below show the purpose of each folder or file:
+
+HES_toolbox/
+├── assets							# styles, logos or repeatedly used files
+│   ├── header.css
+│   ├── logo.png
+│   └── print_setup.css
+├── functions						# stand-alone functions called by other functions
+│   ├── ClassPDF.py						# class file for reporting with FPDF
+│   ├── fun_applyAliasTable.py
+│   ├── fun_generateInvNormPlot.py
+│   ├── fun_generateTableGeneric.py
+│   ├── fun_generateTableOverview.py
+│   ├── fun_generateTimeSeriesPlots.py
+│   ├── fun_generateXYPlot.py
+│   ├── fun_loadData.py
+│   ├── fun_removeDates.py
+│   └── __init__.py
+├── README.md							# Readme file
+├── reportDriver.py						# Driver script for generating reports with fpdf
+├── reportSimpleDASH.py					# Script to generate report with plotly-dash
+├── reportSimplePDF.py					# Script to generate reports with fpdf
+├── reportTemplate.py					# user template file called by `reportDriver.py`
+└── requirements.txt					# package requirements
+
+
 ## Main files
 - `reportSimpleDASH.py`: Run this file with `python3 reportSimpleDASH.py` to produce a simple dashboard report. View in browser at `http://127.0.0.1:8050`. It can be printed to PDF using the browser Print option.
 - `reportSimplePDF.py`: compile a pdf directly with `python3 reportSimplePDF.py`. To change compilation options (including which layout, variables to include in plots, which plots to include, title, author, captions, etc), edit the file `reportSimplePDF.py`.
+- `reportDriver.py`: compile a pdf with the same FPDF package as `reportSimplePDF.py`. With `reportDriver.py` all user input is split into a separate template file (e.g. `reportTemplate.py`) which is loaded by the `reportDriver.py` to compile the PDF. Run with `python3 reportDriver.py`.
 
-### reportSimplePDF.pdf
+### reportSimplePDF.py
 To configure, set the following options in `reportSimplePDF.py`, which is split up into Configuration, Analysis and Compilation sections:
 #### Configuration options
 Logo, metadata and output options:
@@ -43,13 +71,9 @@ aliasTable = {
 Note that I've kept `TIMESTAMP` as `TIMESTAMP`, and order should not matter.
 #### Analysis options
 - set options for the summary table
-
 - set options for any time series plots
-
 - set options for any X-Y plots
-
 - set options for any inverse normal plots
-
 - set options for a largest event table.
 
 #### Compilation options
@@ -64,6 +88,21 @@ pdf.output(output_name, 'F')
 ```
 A more complex template is provided in the default `reportSimplePDF.py`, which includes a table of summary statistics followed by some general notes; 2 plots of timeseries data; 2 plots of X-Y data; 1 inverse normal plot; and, a largest event table followed by some notes. Each section is separated by a section title and a page break.
 
+### reportDriver.py
+This is an alternative method for compiling a PDF using the FPDF library, similar to using `reportSimplePDF.py`. However, this method separates out the driver (that compiles the PDF) from the template file (which contains all user input and definitions).
+
+#### 1. Set the user template file:
+See for example `reportTemplate.py`. 
+
+1. First set the global definitions (e.g. title, author, output_name); the file locations and the names of the columns in the `.dat` file; and, what to alias those names as.
+2. Summary table: Set the title, column names, fields and names to show in the table. Optionally, include some notes about this table.
+3. Time series plots: Set how many plots you want, then include a definition `timeseries_plotX = ["first_variable","etc"]` where `X` is increments for each plot.
+4. XP plots. Same as for Timeseries plots.
+5. Inverse Normal plots. Same as for Timeseries plots. In addition, set the bin increments and fitting limits.
+6. Largest events table: Set the title, column titles and fields.
+
+#### 2. Run `reportDriver.py`
+Now run `python3 reportDriver.py` to load the user definitions and compile the output PDF.
 
 
 ### Credits:
---
+++ reportTemplate.py
@@ -0,0 +1,116 @@
+### Template file
+# This file provides the template for outputting a report with reportDriver.py
+# Settings are defined here and this whole file is loaded as a module when the 
+# driver file is run.
+# If multiple plots are desired, keep the setting name the same, but increment
+# by 1 (e.g. timeseries_plot1, timeseries_plot2)
+###
+
+# global defns
+title = 'Example title that is about the same length '
+author = 'Example author and date'
+logo_path = 'assets/logo.png'
+output_name = 'test.pdf'
+
+# file locations
+SS_filename = '../dash/GAF/Data/HE605_RV50_GAF_SensorStats.dat'
+SRES_filename = '../dash/GAF/Data/HE605_RV50_GAF_SensorRelEventStats.dat'
+
+# names of columns in each file from LoggerNet
+colNamesSS = ["TIMESTAMP","RECORD","BattV_Min","BattV_Avg","PTemp_C_Avg","SensorRel_Min(1)","SensorRel_Min(2)","SensorRel_Min(3)","SensorRel_Min(4)","SensorRel_Min(5)","SensorRel_Max(1)","SensorRel_Max(2)","SensorRel_Max(3)","SensorRel_Max(4)","SensorRel_Max(5)"]
+colNamesSRES = ["TIMESTAMP","RECORD","SensorRelEventMin(1)","SensorRelEventMin(2)","SensorRelEventMin(3)","SensorRelEventMin(4)","SensorRelEventMin(5)","SensorRelEventMax(1)","SensorRelEventMax(2)","SensorRelEventMax(3)","SensorRelEventMax(4)","SensorRelEventMax(5)","SensorRelEventPkp(1)","SensorRelEventPkp(2)","SensorRelEventPkp(3)","SensorRelEventPkp(4)","SensorRelEventPkp(5)","SensorWfmTrigSensor","SensorOffset(1)","SensorOffset(2)","SensorOffset(3)","SensorOffset(4)","SensorOffset(5)"]
+
+# Alias table definition
+aliasTable_SRES = {
+    "TIMESTAMP":"TIMESTAMP",
+    "RECORD":"record",
+    "SensorRelEventMin(1)":"S_P2_CH_L_HJCrack_Min",
+    "SensorRelEventMin(2)":"S_P2_CH_R_HJCrack_Min",
+    "SensorRelEventMin(3)":"S_S2_G1_Min",
+    "SensorRelEventMin(4)":"S_S2_G2_Min",
+    "SensorRelEventMin(5)":"S_S2_G3_Min",
+    "SensorRelEventMax(1)":"S_P2_CH_L_HJCrack_Max",
+    "SensorRelEventMax(2)":"S_P2_CH_R_HJCrack_Max",
+    "SensorRelEventMax(3)":"S_S2_G1_Max",
+    "SensorRelEventMax(4)":"S_S2_G2_Max",
+    "SensorRelEventMax(5)":"S_S2_G3_Max",
+    "SensorRelEventPkp(1)":"S_P2_CH_L_HJCrack_Pkp",
+    "SensorRelEventPkp(2)":"S_P2_CH_R_HJCrack_Pkp",
+    "SensorRelEventPkp(3)":"S_S2_G1_Pkp",
+    "SensorRelEventPkp(4)":"S_S2_G2_Pkp",
+    "SensorRelEventPkp(5)":"S_S2_G3_Pkp",
+    "SensorWfmTrigSensor":"Sensor Waveform Trigger",
+    "SensorOffset(1)":"S_P2_CH_L_HJCrack_Offset",
+    "SensorOffset(2)":"S_P2_CH_R_HJCrack_Offset",
+    "SensorOffset(3)":"S_S2_G1_Offset",
+    "SensorOffset(4)":"S_S2_G2_Offset",
+    "SensorOffset(5)":"S_S2_G3_Offset"
+}
+
+# 1. Summary table title, sensor fields and names
+table_title = '' # title for the table
+col_titles = ['','all time','last week'] # column titles
+sensorStatisticsFields=['S_P2_CH_L_HJCrack_Max',
+                        'S_P2_CH_R_HJCrack_Max',
+                        'S_S2_G1_Max',
+                        'S_S2_G2_Max',
+                        'S_S2_G3_Max'] # what fields to do you want stats for?
+sensorStatisticsNames=['S_P2_CH_L_HJCrack Max',
+                       'S_P2_CH_R_HJCrack Max',
+                       'S_S2_G1 Max',
+                       'S_S2_G2 Max',
+                       'S_S2_G3 Max'] # name to give in table?
+
+
+stats_notes = 'This bit can include any special notes that you want to make about the statistics in this report. e.g. Note erroneous data arising due to blah blah. This will be automatically added as a special note to the end of the statistics summary section.'
+
+# 2. Time series plot - include field names for each plot
+NoTimeSeriesPlots = 1
+timeseries_plot1 = ["SensorRel_Max(1)",
+                    "SensorRel_Max(2)",
+                    "SensorRel_Max(3)",
+                    "SensorRel_Max(4)",
+                    "SensorRel_Max(5)"]
+                    
+# 3. Definitions and notes for x-y section
+NoXYPlots = 2
+XY_plot1_title = 'R vs L HJ Crack'
+XY_plot1 = ['S_P2_CH_R_HJCrack_Pkp',
+            'S_P2_CH_L_HJCrack_Pkp']
+
+XY_plot2_title = 'G1 vs G3 of Span 2'
+XY_plot2 = ['S_S2_G1_Pkp',
+            'S_S2_G3_Pkp']
+
+
+# 4. Definitions and notes for inv normal plot section
+NoInvNormPlots = 1
+title_InvNorm = 'title of inverse normal plot'
+InvNorm_toPlot1 = ["S_P2_CH_R_HJCrack_Pkp",
+                    "S_P2_CH_L_HJCrack_Pkp",
+                    "S_S2_G1_Pkp",
+                    "S_S2_G2_Pkp",
+                    "S_S2_G3_Pkp"]
+InvNormBinStart=0
+InvNormBinEnd=200
+InvNormBinInc=2
+InvNormMin = 1
+InvNormMax = 2.75
+
+
+# 5. Title, column names, fields and field names for the 'largest event' table
+table_title2 = '' # title for the table
+col_titles2 = ['','all time','last week'] # column titles
+sensorStatisticsFields2=['S_P2_CH_L_HJCrack_Pkp',
+                        'S_P2_CH_R_HJCrack_Pkp',
+                        'S_S2_G1_Pkp',
+                        'S_S2_G2_Pkp',
+                        'S_S2_G3_Pkp'] # what fields to do you want stats for?
+sensorStatisticsNames2=['P2_L_HJCrack Pkp',
+                        'P2_R_HJCrack Pkp',
+                        'S2_G1 Pkp',
+                        'S2_G2 Pkp',
+                        'S2_G3 Pkp'] # name to give in table?
+
+stats_notes2 = 'This bit can include any special notes that you want to make about the statistics in this report. e.g. Note erroneous data arising due to blah blah. This will be automatically added as a special note to the end of the statistics summary section.'
+
