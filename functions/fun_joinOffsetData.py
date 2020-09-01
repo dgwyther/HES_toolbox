003b8e902178f71604619c8c026d29dc1018ac0d
David Gwyther <david.gwyther@gmail.com>
2020-09-01 12:15:10 +1000

    added function to re-align offset data

---
+++ functions/fun_joinOffsetData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+def joinOffsetData(inputData,TimeIndex,Variable,shiftTime,shiftValue):
+    for r,var in enumerate(Variable):
+        mask = (inputData[TimeIndex]>=shiftTime[r])
+        inputData[var].where(~mask, other=inputData[var]+shiftValue[r],inplace=True)
+    print('re-aligned and joined '+str(len(Variable))+' variables:')
+    print(Variable)
+    print('by the following amounts: ')
+    print(shiftValue)
