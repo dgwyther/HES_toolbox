d0148e6695eec9776c324c5ad042e0089f184fd6
dgwyther <david.gwyther@gmail.com>
2020-11-26 07:19:11 +1000

    update the nangaps fcn to be more robust

--- functions/fun_addNANGaps.py
+++ functions/fun_addNANGaps.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 def addNANGaps(inputData,TimeIndex,sampleInterval):
+#    r = pd.date_range(start=inputData[TimeIndex].min(), end=inputData[TimeIndex].max(), freq=sampleInterval)
+#    print(r)
+#    inputData.set_index('TIMESTAMP').reindex(r).fillna(0.0).rename_axis('TIMESTAMP').reset_index()
     inputData = inputData.set_index(TimeIndex).asfreq(sampleInterval).reset_index()
     return inputData
